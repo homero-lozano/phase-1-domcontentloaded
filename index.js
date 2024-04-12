c19318998db0a9caeb196dcfbb15f4e7172441ad
Homero Lozano <homeronicolaslozano@gmail.com>
2024-04-12 10:53:17 -0400

    The JavaScript DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('After DOM Loaded')
+    var textElement = document.getElementById('text')
+    textElement.textContent = "This is really cool!"
+})
+
